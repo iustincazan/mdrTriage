cfe71fb2f3133598adeba5e62ac657e3103268fc
Iustin Cazan <iustincazan@yahoo.com>
2025-09-14 13:54:09 +0300

    Initial Commit

---
+++ mdrTriage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mdrTriage
+//
+//  Created by Iustin Cazan on 14.09.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
